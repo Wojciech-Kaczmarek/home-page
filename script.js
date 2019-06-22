c7c9e9ace9586617861de8047c00404d5c6dee33
Wojciech Kaczmarek <wojciechpiotrkaczmarek@gmail.com>
2019-06-22 18:51:50 +0200

    js 00

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-// console.log('dupa')
-// console.error('dupa');
-
-// console.log('%c Dupa!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
-
-// const app = document.getElementById("app");
-// const dupa = app.getElementsByClassName('button-dupa');
-
-// dupa.item(3).innerText = "3D DUpa";
-
-// app.classList.add("super");
-
-//console.log(dupa);
 
 
-// dupa
-// dupa
 
-// dupa
-// dupadupadupadupadupa
-// dupa
-// dupadupa
-// dupadupadupa
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log('%c Dupa!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
