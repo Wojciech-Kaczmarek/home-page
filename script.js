2768491a831048a551c87be0e6ab5891eeeb17ac
Wojciech Kaczmarek <wojciechpiotrkaczmarek@gmail.com>
2019-06-26 11:31:59 +0200

    refactoring 03

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ console.log(`my name is ${name} and my age is ${age}`);
 // string = tekst ' ; number ; bigint => 2*53 ; boolean = true false ; null ; undefined ; symbol
 
 
+console.log('%c hey, kiss me if you can!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
 
 
 
@@ -24,12 +25,4 @@ console.log(`my name is ${name} and my age is ${age}`);
 
 
 
-
-
-
-
-
-
-
-
-//console.log('%c Dupa!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+// console.log('%c Dupa!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
