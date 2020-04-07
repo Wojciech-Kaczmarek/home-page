36a1f50cdb423ad4dc1aaadb522822cb20ac3ae2
Wojciech Kaczmarek <wojciechpiotrkaczmarek@gmail.com>
2020-04-07 23:40:10 +0200

    js variable

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-//alert ('kiss me hard, like really.. really hard');
 
-console.log('kiss me');
+var name = "kiss";
+var place = "hell";
 
-const name = 'kiss hell';
-const age = 1;
+console.log("you are "+name+" and you are from "+place+"")
 
-console.log(name);
-console.log(age);
 
-console.log(`hey, my name is ${name} and i hope you are ready to see my new, beginer project`);
 
-console.log(`my name is ${name} and my age is ${age}`);
+// const name = 'kiss hell';
+// const age = 1;
+
+
+// console.log(`hey, my name is ${name} and i hope you are ready to see my new, beginer project`);
+
+// console.log(`my name is ${name} and my age is ${age}`);
 //console.log('my name is '+name+' and my age is '+age+'');
 
 // string = tekst ' ; number ; bigint => 2*53 ; boolean = true false ; null ; undefined ; symbol
 
-
-console.log('%c hey, kiss me if you can!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
-
-
-
-
-
-
-
-
-// console.log('%c Dupa!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
