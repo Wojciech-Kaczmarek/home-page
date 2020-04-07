024514d9ea329aa6d9b3e47bda9762afd4e93703
Wojciech Kaczmarek <wojciechpiotrkaczmarek@gmail.com>
2020-04-08 00:31:41 +0200

    js tweet exercise

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 
+
+/*var tweet = prompt("Compose your tweet:");
+var tweetCount = tweet.length;
+alert("You have written " + tweetCount + " characters, you have " + (260 - tweetCount) + " characters remaning.")
+*/
+
+var tweet = prompt("Compose your tweet:");
+alert("You have written " + tweet.length + " characters, you have " + (260 - tweet.length) + " characters remaning.")
+
+
+
+// You have written 180 characters, you have -60 characters left.
+
+
+
+
+/*
 var name = "kiss";
 var place = "hell";
 
@@ -13,6 +30,7 @@ console.log('bla ${name} bla')
 console.log(`bla ${name} bla`)
 
 alert(name + " "+"is from" + " " + place)
+*/
 
 
 
