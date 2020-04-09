7c38e845df1625f37739370ea9d4073bc467ba07
Wojciech Kaczmarek <wojciechpiotrkaczmarek@gmail.com>
2020-04-09 10:53:06 +0200

    js functions

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-/*var name = prompt("what is your name:");
+function dogAgeInHumanYears() {
+    var dogAge = prompt("how old is your dog?");
 
-var firstChar = name.slice(0,1);
-var upperCasefirstChar = firstChar.toUpperCase();
-var restOfName = name.slice(1,name.lenth);
-restOfName = restOfName.toLowerCase();
-var capitalizedName = upperCasefirstChar + restOfName;
+    var humanAge = ((dogAge - 2) * 4) + 21;
+    alert("your dog is " + humanAge + " years old in human years");
+}
 
-alert("hello " + capitalizedName);
-*/
+function helloAlert() {
+    var name = prompt("what is your name:");
+
+    var firstChar = name.slice(0, 1);
+    var upperCasefirstChar = firstChar.toUpperCase();
+    var restOfName = name.slice(1, name.lenth);
+    restOfName = restOfName.toLowerCase();
+    var capitalizedName = upperCasefirstChar + restOfName;
+
+    alert("hello " + capitalizedName);
+}
+
+//helloAlert();
 
 // string = tekst ' ; number ; bigint => 2*53 ; boolean = true false ; null ; undefined ; symbol
 
-var dogAge = prompt("what is your dog age?");
-
-var humanAge = ((dogAge - 2) * 4) + 21;
-alert("your dogs age is: " + humanAge);
+function getMilk(battle) {   
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("buy " + battle + " buttle of milk");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+  }
+  
+  getMilk(5);
+  
