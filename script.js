8549ca62f3e50f51932ae2b59dfb323ee6433790
Wojciech Kaczmarek <wojciechpiotrkaczmarek@gmail.com>
2020-04-09 09:48:31 +0200

    js dogAge

--- script.js
+++ script.js
@@ -1,62 +1,17 @@
-/* 
-var tweet = prompt("Compose your tweet:");
-var tweetCount = tweet.length;
-alert("You have written " + tweetCount + " characters, you have " + (260 - tweetCount) + " characters remaning.")
+/*var name = prompt("what is your name:");
+
+var firstChar = name.slice(0,1);
+var upperCasefirstChar = firstChar.toUpperCase();
+var restOfName = name.slice(1,name.lenth);
+restOfName = restOfName.toLowerCase();
+var capitalizedName = upperCasefirstChar + restOfName;
+
+alert("hello " + capitalizedName);
 */
-
-/*
-var tweet = prompt("Compose your tweet:");
-alert("You have written " + tweet.length + " characters, you have " + (140 - tweet.length) + " characters remaning.")
-*/
-
-/*
-var tweet = prompt("Compose your tweet:");
-var TweetUnder140 = tweet.slice(0,140);
-alert(TweetUnder140);
-*/
-
-/*
-var tweet = prompt("Compose your tweet:");
-alert(tweet.slice(0,140));
-*/
-
-alert(prompt("Compose your tweet:").slice(0,140));
-
-
-
-// You have written 180 characters, you have -60 characters left.
-
-
-
-
-/*
-var name = "kiss";
-var place = "hell";
-
-console.log("you are "+name+" and you are from "+place)
-
-console.log(place+" "+name)
-
-// string
-
-console.log("bla ${name} bla")
-console.log('bla ${name} bla')
-console.log(`bla ${name} bla`)
-
-alert(name + " "+"is from" + " " + place)
-*/
-
-
-
-
-// const name = 'kiss hell';
-// const age = 1;
-
-
-// console.log(`hey, my name is ${name} and i hope you are ready to see my new, beginer project`);
-
-// console.log(`my name is ${name} and my age is ${age}`);
-//console.log('my name is '+name+' and my age is '+age+'');
 
 // string = tekst ' ; number ; bigint => 2*53 ; boolean = true false ; null ; undefined ; symbol
 
+var dogAge = prompt("what is your dog age?");
+
+var humanAge = ((dogAge - 2) * 4) + 21;
+alert("your dogs age is: " + humanAge);
