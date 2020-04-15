dc8dab8f81a8eb36b5c78da6d2f11c7c4a66f0af
Wojciech Kaczmarek <wojciechpiotrkaczmarek@gmail.com>
2020-04-15 20:06:45 +0200

    js loveCalculator

--- script.js
+++ script.js
@@ -76,4 +76,12 @@ function bmiCalculator(weight, height) {
     var bmi = weight / (height * height);
     return Math.round(bmi);
 }
+function loveCalculator() {
+prompt("What is your name?");
+prompt("What is their name?");
 
+var loveScore = Math.random();
+loveScore = loveScore * 100;
+loveScore = Math.floor(loveScore) + 1;
+console.log("Your love score is " + loveScore + "%");
+}
