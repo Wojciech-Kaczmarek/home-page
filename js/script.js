17e10b8411bc227c2f87f8b57a07a6a1d0ee8c0f
Wojciech Kaczmarek <wojciechpiotrkaczmarek@gmail.com>
2020-05-27 19:10:36 +0200

    function calculate

--- js/script.js
+++ js/script.js
@@ -2,4 +2,10 @@
 const heading = document.querySelector(".main__header-js");
 heading.innerHTML = "Black Hole";
 
-document.querySelector(".main__header-description--js").innerHTML = "kiss";
+document.querySelector(".main__header-description--js").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam id voluptates eveniet, maxime quaerat vero pariatur aliquam illum suscipit iure beatae vel veniam porro sequi, ut unde quis tempore in.";
+
+function calculate(myNumber) {
+    console.log(`Dostałem ${myNumber}`);
+    myNumber = myNumber * 7;
+    return myNumber
+}
