050d632ef101c8ca29736508bd1c9d5bbe40dca7
Wojciech Kaczmarek <wojciechpiotrkaczmarek@gmail.com>
2020-07-01 22:03:07 +0200

    add menu button: sunday

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 console.time("answer time");
 
 
+/* navigation */
 
+const menu = document.querySelector('.menu--js');
+
+menu.addEventListener('click', () => {
+    const nav = document.querySelector('.navigation--js');
+    nav.classList.toggle('navigation--open');
+})
+
+/* navigation */
 
 const greet = (age, name) => {
 
@@ -84,14 +93,6 @@ showMeProperty('isOperating');
 
 console.timeEnd("answer time");
 
-/* navigation */
-
-const menu = document.querySelector('.menu--js');
-
-menu.addEventListener('click', () => {
-    const nav = document.querySelector('.navigation--js');
-    nav.classList.toggle('navigation--open');
-})
 
 /*
  console.time("answer time");
